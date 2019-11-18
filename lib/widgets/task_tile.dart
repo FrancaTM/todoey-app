9f9c1ccaabfb31a7bc17f8e6f0995e1686ba3907
FrancaTM <francatm@outlook.com>
2019-11-18 19:34:12 -0300

    refactor taskTile and create taskCheckbox

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,21 +1,54 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   final String task;
 
   const TaskTile({this.task});
 
   @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        task,
+        widget.task,
         style: TextStyle(
           fontSize: 20.0,
           fontWeight: FontWeight.bold,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: Checkbox(value: false, onChanged: (value) {}),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: checkboxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
